1100fbe20aca2e6f6ad9388543d54af39c5ac150
Justin Welsch <j.welsch@gmail.com>
2014-12-17 09:33:20 -0500

    Setting up file copy engine

---
+++ ImageCopy/CLArguments.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CommandLineLib;
+
+namespace ImageCopy
+{
+   internal enum SortCriteria
+   {
+      DateTaken
+   }
+
+   internal class CLArguments
+   {
+      [DirectoryPathCompound( "-source", MustExist = true, Description = "Path to the directory containing image files to copy." )]
+      public string Source
+      {
+         get;
+         private set;
+      }
+
+      [DirectoryPathCompound( "-target", Description = "Path to the directory where the images will be copied." )]
+      public string Target
+      {
+         get;
+         private set;
+      }
+
+      [EnumCompound( "-sort", Description = "Criteria by which to sort the images." )]
+      public SortCriteria Sort
+      {
+         get;
+         private set;
+      }
+
+      [Switch( "-overwrite", Optional = true, Description = "Include to automatically overwrite files in the target directory." )]
+      public bool Overwrite
+      {
+         get;
+         private set;
+      }
+   }
+}
--- ImageCopy/Program.cs
+++ ImageCopy/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using CommandLineLib;
+using FileCopyLib;
+using ExifLib;
 
 namespace ImageCopy
 {
@@ -6,6 +9,55 @@ namespace ImageCopy
    {
       static void Main( string[] args )
       {
+         CommandLine<CLArguments> commandLine = null;
+
+         try
+         {
+            var arguments = commandLine.Parse( args );
+
+            var copier = new FileCopier();
+            copier.NextFile += ( sender, e ) =>
+               {
+                  using ( var reader = new ExifReader( e.SourceFile ) )
+                  {
+                     DateTime dateTaken;
+                     if ( reader.GetTagValue<DateTime>( ExifTags.DateTimeDigitized, out dateTaken ) )
+                     {
+                     }
+                  }
+               };
+         }
+         catch ( CommandLineException ex )
+         {
+            System.Diagnostics.Trace.WriteLine( ex );
+
+            if ( commandLine == null )
+            {
+               Console.WriteLine( ex );
+            }
+            else
+            {
+               Console.WriteLine( commandLine.Help() );
+            }
+         }
+         catch ( CommandLineDeclarationException ex )
+         {
+            System.Diagnostics.Trace.WriteLine( ex );
+
+            if ( commandLine == null )
+            {
+               Console.WriteLine( ex );
+            }
+            else
+            {
+               Console.WriteLine( commandLine.Help() );
+            }
+         }
+         catch ( Exception ex )
+         {
+            System.Diagnostics.Trace.WriteLine( ex );
+            Console.WriteLine( ex );
+         }
       }
    }
 }
